b3b96cacc239c71f39a28c50be53e7bbee74463f
dirtyhabits97 <gonzalorehu@gmail.com>
2021-08-01 22:18:06 -0500

    Added TokenType

---
+++ Sources/slox/TokenType.swift
@@ -0,0 +1,25 @@
+//
+//  File.swift
+//  
+//
+//  Created by user on 1/08/21.
+//
+
+import Foundation
+
+enum TokenType {
+    // Single-character tokens
+    case LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE, COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR
+
+    // One or two character tokens
+    case BANG, BANG_EQUAL, EQUAL, EQUAL_EQUAL, GREATER, GREATER_EQUAL, LESS, LESS_EQUAL
+
+    // Literals
+    case IDENTIFIER, STRING, NUMBER
+
+    // Keywords
+    case AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR, PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE
+
+    // End of file
+    case EOF
+}
